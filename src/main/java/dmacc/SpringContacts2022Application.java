63ea08b32044861468fc26962fee11a81d40fa95
Dominic Wood <ddwood87@yahoo.com>
2022-11-09 19:32:19 -0600

    formatted input form and result table

--- src/main/java/dmacc/SpringContacts2022Application.java
+++ src/main/java/dmacc/SpringContacts2022Application.java
@@ -1,11 +1,9 @@
 package dmacc;
-
+/*
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
@@ -14,17 +12,22 @@ import dmacc.beans.Address;
 import dmacc.beans.Contact;
 import dmacc.controller.BeanConfiguration;
 import dmacc.repository.ContactRepository;
+*/
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringContacts2022Application{
-	//@Autowired
-	//ContactRepository repo;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringContacts2022Application.class, args);
 	}
 	
-	/*
+/*
+	ContactRepository repo;
+	@Autowired
+	 
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
@@ -32,7 +35,7 @@ public class SpringContacts2022Application{
 		//Contact c = appContext.getBean("contact", Contact.class);
 		//c.setRelationship("best friend");
 		//repo.save(c);
-		//Contact d = new Contact("Sadra Boynton", "555-555-5557", "friend");
+		//Contact d = new Contact("Sandra Boynton", "555-555-5557", "friend");
 		//Address a = new Address("123 Main Street", "Des Moines", "IA");
 		//d.setAddress(a);
 		//repo.save(d);
@@ -42,5 +45,5 @@ public class SpringContacts2022Application{
 		}
 		((AbstractApplicationContext) appContext).close();
 	}
-	*/
+*/
 }
